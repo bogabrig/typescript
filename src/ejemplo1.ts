c746f8daa934924f571653bbbcfa1fff3c77d103
bogabrig <gabrielegarcia@yahoo.com.ar>
2025-02-10 11:41:16 -0300

    nuevo repositorio con el ejercicio de typescript

---
+++ src/ejemplo1.ts
@@ -0,0 +1,9 @@
+let nombre = "Gabriel";
+let apellido = "Hernandez";
+//console.log(`Mi nombre es ${nombre} ${apellido}`);
+function sumar(a: number, b: number) {
+  return a + b;
+}
+sumar(2, 3);
+
+console.log(sumar(2, 4)); 
